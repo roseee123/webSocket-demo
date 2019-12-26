2efb81c5076043925682f37d2d80d366cc3a576d
Roseee123 <m10702150@gapps.ntust.edu.tw>
2019-12-26 15:58:48 +0800

    server connect one client

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const SocketServer = require('ws').Server;
+
+const PORT = 3000;
+
+//創建 express 的物件，並綁定及監聽 3000 port ，且設定開啟後在 console 中提示
+const server = express().listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+//將 express 交給 SocketServer 開啟 WebSocket 的服務
+const wss = new SocketServer({ server });
+
+//當 WebSocket 從外部連結時執行
+wss.on('connection', ws => {
+    console.log('Client connected');
+
+    //固定送最新時間給 Client
+    const sendNowTime = setInterval(() => {
+        ws.send(String(new Date()))
+    },1000);
+
+    //對 message 設定監聽，接收從 Client 發送的訊息
+    ws.on('message', data => {
+        ws.send(data);
+    });
+
+    //當 WebSocket 的連線關閉時執行
+    ws.on('close', () => {
+        console.log('Close connected');
+    });
+});
